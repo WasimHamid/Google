5392c6eb082d98273cc93a4d8149e35aff7d629e
Your Name <you@example.com>
2019-04-12 15:16:37 +0100

    changed the p tag

--- src/App.js
+++ src/App.js
@@ -18,9 +18,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>this is Wasim Hamid's React WebPage</p>
           <a
             className="App-link"
             href="https://reactjs.org"
